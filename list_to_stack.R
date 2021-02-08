65bc10f88b61e13328dd2a08cd3ac656517750e3
Virginia Morera-Pujol <morera.virginia@gmail.com>
2021-02-08 16:30:38 +0100

    Update list_to_stack.R

--- list_to_stack.R
+++ list_to_stack.R
@@ -1,10 +1,8 @@
 list_to_stack <- function(raster_list) {
   require(raster)
   ext_list <- lapply(X = raster_list, function(x) {as.matrix(x@extent)})
-  
   matrix_extent <- matrix(unlist(ext_list), ncol=length(ext_list))
   rownames(matrix_extent)<-c("xmin", "ymin", "xmax", "ymax")
-  
   # create an extent with the extrem values of your extent
   best_extent <- extent(min(matrix_extent[1,]), max(matrix_extent[3,]), 
                         min(matrix_extent[2,]), max(matrix_extent[4,]))
@@ -14,5 +12,4 @@ list_to_stack <- function(raster_list) {
                       raster(ext = best_extent, crs = ras_list[[1]]@crs, resolution = min_res))
   res_stack <- stack(res_list2)
   return(res_stack)
-
   }
