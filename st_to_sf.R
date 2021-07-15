5db215f02e6a489974f21e8b056bb89697619b32
Virginia Morera-Pujol <morera.virginia@gmail.com>
2021-07-15 13:13:10 +0100

    Update st_to_sf.R
    Make function magrittr pipe friencly

--- st_to_sf.R
+++ st_to_sf.R
@@ -1,13 +1,13 @@
-sf_to_df <- function(dat, projected = FALSE) {
+sf_to_df <- function(.dat, projected = FALSE) {
   if(projected) {
     x2 <- x %>% 
-      mutate(x = unlist(map(dat$geometry,1)),
-             y = unlist(map(dat$geometry,2))) %>% 
+      mutate(x = unlist(map(.dat$geometry,1)),
+             y = unlist(map(.dat$geometry,2))) %>% 
       st_set_geometry(NULL)} else {
     
     x2 <- x %>% 
-      mutate(Longitude = unlist(map(dat$geometry,1)),
-             Latitude = unlist(map(dat$geometry,2))) %>% 
+      mutate(Longitude = unlist(map(.dat$geometry,1)),
+             Latitude = unlist(map(.dat$geometry,2))) %>% 
       st_set_geometry(NULL)}
   return(x2)
 }
